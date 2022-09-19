55a84e72c77f9a8f9c34d254fde525eeb56df466
Ragnarok <deathmetalldrummer@gmail.com>
2022-09-19 23:05:24 +0600

    ?

--- source/devel/index.js
+++ source/devel/index.js
@@ -1,1 +1,12 @@
 //#include("./_layout/_mounting.js");
+$('.stylesControl__').on('click', function(e) {
+	console.log($(e.target).data('style'));
+	$('body')
+	.removeClass('style_1')
+	.removeClass('style_2')
+	.removeClass('style_3')
+	.removeClass('style_4')
+	.removeClass('style_5')
+	.removeClass('style_6')
+	.addClass($(e.target).data('style'));
+})
